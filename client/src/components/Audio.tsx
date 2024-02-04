c79e9fab49f0b8ae067bc3cc3cd36ba938af5ae0
alexanderh2023F <149343875+alexanderh2023F@users.noreply.github.com>
2024-02-03 20:37:53 -0500

    pushing my code

---
+++ client/src/components/Audio.tsx
@@ -0,0 +1,85 @@
+import { useState, useEffect, useRef, FormEvent } from "react";
+import { HiForward, HiBackward, HiPause, HiPlay } from "react-icons/hi2";
+import GentleBreeze from "/src/assets/audio/agentlebreeze.mp3";
+import DreamingSeas from "/src/assets/audio/dreamingseas.mp3";
+import EveningSakura from "/src/assets/audio/eveningsakura.mp3";
+import LoveSerenade from "/src/assets/audio/loveserenade.mp3";
+import MeditativeRain from "/src/assets/audio/meditativerain.mp3";
+import ReflectedLight from "/src/assets/audio/reflectedlight.mp3";
+import WhiteNoise from "/src/assets/audio/whitenoise.mp3";
+import "../styles/AudioS.css";
+
+const Audio: React.FC = () => {
+  const [play, setPlay] = useState(true);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const songs = [
+    {
+      title: "A Gentle Breeze",
+      src: GentleBreeze
+    },
+    {
+      title: "Dreaming Seas",
+      src: DreamingSeas
+    },
+    {
+      title: "Evening Sakura",
+      src: EveningSakura
+    },
+    {
+      title: "Love Serenade",
+      src: LoveSerenade
+    },
+    {
+      title: "Meditative Rain",
+      src: MeditativeRain
+    },
+    {
+      title: "Reflected Light",
+      src: ReflectedLight
+    },
+    {
+      title: "White Noise",
+      src: WhiteNoise
+    }
+  ];
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const currentSong = songs[currentSongIndex];
+
+  const playPauseHandler = () => {
+    if (play) {
+      audioRef.current?.pause();
+      setPlay(false);
+    } else {
+      audioRef.current?.play();
+      setPlay(true);
+    }
+  };
+  const songEndHandler = () => {
+    setCurrentSongIndex((currentSongIndex + 1) % songs.length);
+    setPlay(true);
+  };
+
+  const previousSongHandler = () => {
+    setCurrentSongIndex((currentSongIndex - 1) % songs.length);
+    setPlay(true);
+  };
+
+  return (
+    <>
+      <audio ref={audioRef} src={currentSong.src} onEnded={songEndHandler}></audio>
+      <div className={"button"}>
+        <button onClick={previousSongHandler}>
+          <HiBackward />
+        </button>
+        <button onClick={playPauseHandler}>{play ? <HiPause></HiPause> : <HiPlay></HiPlay>}</button>
+        <button onClick={songEndHandler}>
+          <HiForward />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Audio;
