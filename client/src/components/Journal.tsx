4533ae53568dda6261213fd8fa312497b46fa113
Andy Lin <linandy1108@gmail.com>
2024-02-04 01:32:27 -0500

    final change hopefully

--- client/src/components/Journal.tsx
+++ client/src/components/Journal.tsx
@@ -4,17 +4,28 @@ import backgroundImage from "../assets/images/journalbg1.jpg";
 import "../styles/JournalStyles.css";
 
 const Journal: React.FC = () => {
-  const [inputText, setInputText] = useState("");
+  const [inputTitleText, setInputTitleText] = useState("");
+  const [inputBodyText, setInputBodyText] = useState("");
   const [currentDate] = useState(new Date());
   const navigate = useNavigate();
 
-  function handleInputChange(e: ChangeEvent<HTMLTextAreaElement>) {
+  function handleInputTitleChange(e: ChangeEvent<HTMLTextAreaElement>) {
     e.preventDefault();
-    setInputText(e.target.value);
+    setInputTitleText(e.target.value);
+  }
+
+  function handleInputBodyChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    e.preventDefault();
+    setInputBodyText(e.target.value);
   }
 
   function handleNavigateToWelcome() {
-    navigate(-1); 
+    if (inputTitleText.length === 0 || inputTitleText.length === 0) {
+      alert("Please do not leave either the title or body text input field blank!");
+    } else {
+      // Axios Post Call (Goes Here)
+      navigate(-1);
+    }
   }
 
   return (
@@ -22,7 +33,7 @@ const Journal: React.FC = () => {
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
-        height: "100vh", // Set an appropriate height
+        height: "100vh", 
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
@@ -31,13 +42,28 @@ const Journal: React.FC = () => {
     >
       <h1 style={{ color: '#333', marginTop: '-10px'}}>My Journal</h1>
       <p style={{ color: '#333', fontSize: '2em', textAlign: 'center', marginTop: '-25px' }}>
-        
         Current Date: {currentDate.toLocaleDateString()}
-        </p>
+      </p>
+      
+      <textarea
+      value={inputTitleText}
+      onChange={handleInputTitleChange}
+      placeholder={'Add title here'}
+      style={{
+          width: '1200px',
+          height: '50',
+          background: 'rgba(255, 192, 203, 0.01)',
+          border: '0px solid #ccc',
+          padding: '10px',
+          color: '#333',
+          fontFamily: 'Helvetica, sans-serif',
+          fontSize: '2em',
+        }}
+      />
 
       <textarea
-        value={inputText}
-        onChange={handleInputChange}
+        value={inputBodyText}
+        onChange={handleInputBodyChange}
         placeholder={'Add text here'}
         style={{
           width: '1200px',
