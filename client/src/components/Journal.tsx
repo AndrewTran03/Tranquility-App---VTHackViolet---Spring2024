618b809f281199d937e97550e9b0a81720c665c2
Andyl04 <158757555+AndyL004@users.noreply.github.com>
2024-02-04 00:42:40 -0500

    Added Andy's functionality of Journal entries

--- client/src/components/Journal.tsx
+++ client/src/components/Journal.tsx
@@ -1,30 +1,59 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/images/journalbg1.jpg";
+import "../styles/JournalStyles.css";
 
 const Journal: React.FC = () => {
   const [inputText, setInputText] = React.useState('');
   const [currentDate] = React.useState(new Date());
   const navigate = useNavigate();
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  function handleInputChange (e: ChangeEvent<HTMLTextAreaElement>) {
+    e.preventDefault();
     setInputText(e.target.value);
-  };
+  }
 
-  const handleNavigateToWelcome = () => {
-    navigate("/welcome");
-  };
+  function handleNavigateToWelcome() {
+    navigate(-1); 
+  }
 
   return (
-    <div style={{ background: '#f0f0f0', padding: '50px' }}>
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        height: "100vh", // Set an appropriate height
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <h1 style={{ color: '#333'}}>My Journal</h1>
-      <p style={{ color: '#333'}}>Current Date: {currentDate.toLocaleDateString()}</p>
+      <p style={{ color: '#333', fontSize: '2em' , textAlign: 'center'}}>
+        
+        Current Date: {currentDate.toLocaleDateString()}
+        </p>
+
       <textarea
-        value={ inputText}
+        value={inputText}
         onChange={handleInputChange}
-        style={{ width: '300px', height: '150px', background: '#ffc0cb', border: '1px solid #ccc', padding: '10px', color: '#333' }}
+        style={{
+          width: '1200px',
+          height: '500px',
+          background: 'rgba(255, 192, 203, 0.01)',
+          border: '0px solid #ccc',
+          padding: '10px',
+          color: '#333',
+          fontFamily: 'Helvetica, sans-serif',
+          fontSize: '2em',
+        }}
       />
-      <button onClick={handleNavigateToWelcome}
-        style={{ position: 'absolute', top: '540px', right: '730px', padding: '8px' }}>
+
+      <button
+        onClick={handleNavigateToWelcome}
+        style={{ padding: '6px', marginBottom: '20px', border: '0 px solid #ccc'}}
+        >
         Go back
       </button>
     </div>
